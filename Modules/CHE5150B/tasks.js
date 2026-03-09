f014cd2a7e5ec35e24dd1a251d8b73e9ffa13681
Stephenson-Lab <m.stephenson1@uea.ac.uk>
2026-03-09 18:29:07 +0000

    Update tasks.js

--- Modules/CHE5150B/tasks.js
+++ Modules/CHE5150B/tasks.js
@@ -1,1 +1,28 @@
+// Edit these 10 entries.
+// answersSmiles can contain multiple acceptable products.
+window.TASKS = {
+  1: {
+  title: "How to Use Mech-Checker - Practice: Draw benzoic acid",
+  formula: "C7H6O2",
+  requireStereo: false,
+  prompt: `How to Use Mech-Checker
 
+Mech-Checker is a formative self-assessment tool designed to support practice questions in which the answer is a chemical structure.
+
+The tool was originally developed by Dr Stephenson to check students’ answers to mechanism questions, hence the name Mech-Checker. It may also be used for other structure-based questions (e.g. spectroscopy or reaction outcome problems).
+
+Read the question prompt carefully in the content block above. Based on the information provided, draw your proposed structure in the editor.
+
+When you are satisfied with your answer:
+
+Click “Check” to receive automated feedback.
+
+Click “Clear” to remove the feedback message (this does not delete your structure).
+
+The tool evaluates your response against the expected answer for the task, checking molecular formula, bonding connectivity, and stereochemistry (where required).
+
+You may attempt the activity multiple times. This tool is intended for formative practice only.`,
+  answersSmiles: ["O=C(O)c1ccccc1"]
+},
+  2: { title: "CHE-5150B - Q1", formula: "C4H8O2",  requireStereo: false, prompt: "What is structure of the missing product on Blackboard", answersSmiles: ["CCOC(C)=O"] },
+};
